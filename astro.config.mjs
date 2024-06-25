666bff5e6a70b3eafea3d64f1ae152db8b70a67a
Akash <akash.gupta@travelopia.com>
2024-06-25 20:51:13 +0530

    Adding changes for GH pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://actuallyakash.github.io',
+  	base: 'starlight-example',
 	integrations: [
 		starlight({
 			title: 'Wordpress Engineering Handbook',
